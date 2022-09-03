626ceac05ec9b8b4699fb7777f132d8613b359bf
George Nady <gorgonady.gn@gmail.com>
2022-09-03 14:58:13 +0200

    init

---
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -0,0 +1,28 @@
+package com.udacity.project4
+
+import android.app.Application
+import com.udacity.project4.di.myModule
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
+import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        /**
+         * use Koin Library as a service locator
+         */
+        startKoin {
+            androidContext(this@MyApp)
+            modules(listOf(myModule))
+        }
+    }
+}
